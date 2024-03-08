d1da888c1f50f03d8e018d555bb26ad282d59079
Imran Ansari <askimranansari@gmail.com>
2024-03-08 19:51:03 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import STodo from './components/ProjectTwo/STodo'
+import Todo from './components/ProjectOne/Todo'
+
+function App() {
+  return (
+    <div>
+    <Todo />
+    <STodo/>
+    </div>
+  )
+}
+
+export default App
